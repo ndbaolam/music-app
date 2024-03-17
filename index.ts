a332ab30f1e40fba1e2b62fdd2710ecc2df25db0
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-03-17 21:59:50 +0700

    Setup Pug

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response, Express} from 'express';
+
+const app: Express = express();
+const port: number = 3000;
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/topics', (req: Request, res: Response) => {
+  res.render('client/pages/topics/index');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
