ec7d0230f9d49241322d4bbfde3821f3abbedc9e
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-03-22 09:42:58 +0700

    Giao dien trang create

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -1,13 +1,36 @@
 import { Request, Response } from "express";
-import Song from "../../models/song.model";
 
+import Song from "../../models/song.model";
+import Topic from "../../models/topic.model";
+import Singer from "../../models/singer.model";
+
+// [GET] /admin/songs/
 export const index = async (req: Request, res: Response) => {
     const songs = await Song.find({
         deleted: false
     });
 
-    res.render("client/pages/songs/index", {
+    res.render("admin/pages/songs/index", {
         pageTitle: "Songs",
         songs: songs
+    });
+}
+
+// [GET] /admin/songs/create
+export const create = async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        deleted: false,
+        status: "active"
+    }).select("title id");
+
+    const singers = await Singer.find({
+        deleted: false,
+        status: "active"
+    }).select("fullName id");
+    
+    res.render("admin/pages/songs/create", {
+        pageTitle: "Thêm mới bài hát",
+        topics: topics,
+        singers: singers
     });
 }
---
+++ public/admin/js/script.js
@@ -0,0 +1,16 @@
+//Preview upload Image
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage) {
+  const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+  const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", (e) => {
+    console.log(e);
+    if (e.target.files.length) {
+      const image = URL.createObjectURL(e.target.files[0]);
+
+      uploadImagePreview.src = image;
+    }
+  });
+}
+//End preview upload image
