462870672db0742d369d29c2c0dbccf31d3ce6cd
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-03-23 21:48:18 +0700

    Play audio input

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -14,3 +14,37 @@ if(uploadImage) {
   });
 }
 //End preview upload image
+
+//Show alert
+const showAlert = document.querySelector('[show-alert]');
+if(showAlert){
+    const time = parseInt(showAlert.getAttribute('data-time'));
+    setTimeout(() => {
+        showAlert.classList.add('alert-hidden');
+    }, time);
+
+    const closeAlert = document.querySelector('[close-alert]');
+    closeAlert.addEventListener('click', () => {
+        showAlert.classList.add('alert-hidden');
+    });
+}
+//End Show alert
+
+//Preview upload Image
+const uploadAudio = document.querySelector("[upload-audio]");
+if(uploadAudio) {
+  const uploadAduioInput = uploadAudio.querySelector("[upload-audio-input]");
+  const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]");
+  const source = uploadAudioPlay.querySelector("source");
+
+  uploadAduioInput.addEventListener("change", (e) => {
+    console.log(e);
+    if (e.target.files.length) {
+      const audio = URL.createObjectURL(e.target.files[0]);
+
+      source.src = audio;
+      uploadAudioPlay.load();
+    }
+  });
+}
+//End preview upload image
