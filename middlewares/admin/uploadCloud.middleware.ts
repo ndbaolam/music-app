b292e6016a414c68e05b8edc880eaffad00e6159
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-03-23 21:39:42 +0700

    upload file audio

--- middlewares/admin/uploadCloud.middleware.ts
+++ middlewares/admin/uploadCloud.middleware.ts
@@ -41,3 +41,26 @@ export const uploadSingle = async (req: Request, res: Response, next: NextFuncti
   
     next();
 };
+
+export const uploadFields = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    //console.log(req["files"])
+    for (const key in req["files"]) { //Lap qua cac key cua object
+      req.body[key] = [];
+
+      const array = req['files'][key];
+      for (const item of array) {
+        try {
+          const result = await uploadToCloudinary(item.buffer);
+          req.body[key].push(result);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  next();
+};
